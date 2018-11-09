875955e5a54b0c9167b59edef00ea87467bfeea1
Rahul Kumar <rahul.kumar@airfi.aero>
2018-11-09 15:58:22 +0530

    swift lint build resolution

--- TravisSetUpDemoTests/TravisSetUpDemoTests.swift
+++ TravisSetUpDemoTests/TravisSetUpDemoTests.swift
@@ -22,10 +22,9 @@ class TravisSetUpDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let a = 5
-        let b = 6
-        
-        XCTAssertEqual(a+b, 11)
+        let abc = 5
+        let bcd = 6
+        XCTAssertEqual(abc+bcd, 11)
     }
 
     func testPerformanceExample() {
